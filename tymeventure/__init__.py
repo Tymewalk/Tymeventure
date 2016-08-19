697196ba532a1a49e398abf07039c121ac7806af
Tymewalk <Tymewalk@users.noreply.github.com>
2016-08-19 12:05:53 -0500

    Fix stdscr crash Back to stable.

--- tymeventure/__init__.py
+++ tymeventure/__init__.py
@@ -1,12 +1,12 @@
 # TYMEVENTURE v0.1.2
-# Status: Experimental
+# Status: Stable
 # A simple unicurses-based game running in Python 3.
 # Help would be appreciated if you know how.
 
 import pickle, os, sys
 import unicurses
 
-version = "0.1.2-devEXP"
+version = "0.1.2-dev"
 hasSave = False
 currentLocation = None
 
@@ -30,16 +30,15 @@ def nextMenu(screen):
     screen.refresh()
     return key
 
-def drawBoxMenu(options, xsize=40):
-    global stdscr
-    stdscr.addstr(0, 0, "-" * 40, unicurses.color_pair(0) | unicurses.A_BOLD)
+def drawBoxMenu(screen, options, xsize=40):
+    screen.addstr(0, 0, "-" * 40, unicurses.color_pair(0) | unicurses.A_BOLD)
     ypos = 1
     for option in options:
-        stdscr.addstr(ypos, 0, option + " " * (xsize - len(option)), unicurses.color_pair(0) | unicurses.A_BOLD)
-        stdscr.addstr(ypos, xsize, "|", unicurses.color_pair(0) | unicurses.A_BOLD)
+        screen.addstr(ypos, 0, option + " " * (xsize - len(option)), unicurses.color_pair(0) | unicurses.A_BOLD)
+        screen.addstr(ypos, xsize, "|", unicurses.color_pair(0) | unicurses.A_BOLD)
         ypos += 1
         
-    stdscr.addstr(ypos, 0, "-" * xsize, unicurses.color_pair(0) | unicurses.A_BOLD)
+    screen.addstr(ypos, 0, "-" * xsize, unicurses.color_pair(0) | unicurses.A_BOLD)
 
 # Set up the world
 locations = list()
@@ -295,7 +294,7 @@ def gameLoop(stdscr):
                     checkItem = False
 
             if checkItem:
-                drawBoxMenu(["(1) Take Item"])
+                drawBoxMenu(stdscr, ["(1) Take Item"])
                 choice = nextMenu(stdscr)
                 # Number doesn't matter here, I'm not converting it to int or anything
                 if choice == "1":
@@ -330,7 +329,7 @@ def gameLoop(stdscr):
                     checkItem = False
 
             if checkItem:
-                drawBoxMenu(["(1) Look At Item", "(2) Drop Item", "(3) Use Item"])
+                drawBoxMenu(stdscr, ["(1) Look At Item", "(2) Drop Item", "(3) Use Item"])
                 choice = nextMenu(stdscr)
                 # Number doesn't matter here, I'm not converting it to int or anything
                 if choice == "1":
